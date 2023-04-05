9c8f57450a6cd85b31c11cb0cbc774325908a48a
AcidePoulain <acide@poulain.systems>
2023-04-05 19:23:24 +0200

    Added an A*-ish pathfinding for the mobs

--- godot/Scenes/characters/ai/FollowTrait.cs
+++ godot/Scenes/characters/ai/FollowTrait.cs
@@ -1,4 +1,7 @@
-﻿using Godot;
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Godot;
 
 namespace DungeonCrawlerJam2023.Scenes.characters.ai;
 
@@ -26,7 +29,59 @@ public class FollowTrait : IAITrait
 
     private Vector2I GetNextNodeAStar(Vector2I start, Vector2I end, params Vector2I[] nodes)
     {
-        // TODO
+        var closed = new List<Vector2I>();
+        var open = new PriorityQueue<Vector2I, int>();
+        open.Enqueue(start, 0);
+
+        // cheating a bit - the cost will always be 1 more than the previous position.
+        // all tiles have the same cost. So i simply increment the cost every loop.
+        int cost = 0;
+        while (open.Count > 0)
+        {
+            var u = open.Dequeue();
+            if (u.X == end.X && u.Y == end.Y)
+            {
+                return closed.First();
+            }
+
+            Vector2I[] possible_neighbors = new Vector2I[4]
+            {
+                new Vector2I(u.X, u.Y + 1),
+                new Vector2I(u.X, u.Y - 1),
+                new Vector2I(u.X + 1, u.Y),
+                new Vector2I(u.X - 1, u.Y)
+            };
+
+            foreach (Vector2I neighbor in possible_neighbors)
+            {
+                // These gymnastics are needed because
+                // I can not check if a position is in our openlist.
+                // This is because open is of type PriorityQueue, which
+                // does not allow the use of Contains() directly on it.
+                var unorderedOpens = open.UnorderedItems.ToList()
+                                                        .Select(_ => _.Item1)
+                                                        .ToList();
+
+                // check this is a walkable tile
+                if (nodes.Contains(neighbor))
+                {
+                    // check that we did not already walk on this
+                    if (!(closed.Contains(neighbor) || (unorderedOpens.Contains(neighbor))))
+                    {
+                        // Do not call Sqrt(). We do not need to know the "real" distance,
+                        // knowing that a point will be farther or closer is enough.
+                        int distance = (int)Mathf.Pow((neighbor.X - end.X), 2) + (int)Mathf.Pow((neighbor.Y - end.Y), 2);
+                        open.Enqueue(neighbor, cost + distance);
+                    }
+                }
+            }
+
+            closed.Add(u);
+            cost++;
+        };
+
+        // TODO this should not be reachable. We have to
+        // raise an exception, or crash, or do something.
         return start;
     }
 }
